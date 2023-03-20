cdd56d278087540917344c82db6f897276c49152
Arjun <iamarjunkharal@gmail.com>
2023-03-20 22:47:40 +0545

    test

--- django2/pages/tests.py
+++ django2/pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+#from django.test import TestCase # we use this while we are using actual data base 
+from django.test import SimpleTestCase # we use this since we are not using any data base
 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200) # here 200 is standard response for successfull http request
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
