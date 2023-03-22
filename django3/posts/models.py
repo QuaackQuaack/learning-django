0d99ef0b98761273a3b5b97da175b1dec533b1ec
Arjun <iamarjunkharal@gmail.com>
2023-03-22 22:02:08 +0545

    home page

--- django3/posts/models.py
+++ django3/posts/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Post(models.Model): # here we have created database model called Post
     text = models.TextField() # it stores text 
+
+    def __str__(self):
+        return self.text[:50]
