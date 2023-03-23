518c1473ade55a1daa900c0fd2553bfb5a2da159
Arjun <iamarjunkharal@gmail.com>
2023-03-23 13:35:05 +0545

    update on test

--- django3/posts/tests.py
+++ django3/posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 # Create your tests here.
 
@@ -10,3 +11,20 @@ class PostModelsTest(TestCase):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name,'just a text')
+
+class HomePageView(TestCase):
+    def setUp(self):
+        Post.objects.create(text= 'this is another text')
+
+    def test_view_url_exits_at_proper_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200),
+        self.assertTemplateUsed(response, 'home.html')
