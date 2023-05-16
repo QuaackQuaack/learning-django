345719f51fdf960a9848e99859854100d9d59bec
Arjun <iamarjunkharal@gmail.com>
2023-05-16 20:27:37 +0545

    minor change

--- django4.0.1/blog/tests.py
+++ django4.0.1/blog/tests.py
@@ -1,66 +1,78 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
 
 from .models import Post
 
-class TestBlog(TestCase):
 
+class BlogTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = get_user_model().objects.create(
-                username = 'a good name', email = 'ok@ok.com', password = 'agood')
-        
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
         cls.post = Post.objects.create(
-                title = 'a good title',
-                body = 'a good body',
-                author = cls.user)
+            title="A good title",
+            body="Nice body content",
+            author=cls.user,
+        )
 
-    def test_model_Post(self):
-        self.assertEqual(self.post.title, "a good title")
-        self.assertEqual(self.post.body, "a good body")
-        self.assertEqual(self.post.author.username, 'a good name')
-        self.assertEqual(str(self.post), 'a good title')
-        self.assertEqual(self.post.get_absolute_url(), "/1/")
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "Nice body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
 
-    def test_url_exit_at_correct_location(self):
+    def test_url_exists_at_correct_location_listview(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page(self):
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nice body content")
         self.assertTemplateUsed(response, "home.html")
-        self.assertContains(response, "a good title")
 
-    def test_blog_page(self):
-        response = self.client.get(reverse("page", kwargs = {"pk": self.post.id }))
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'page.html')
-        self.assertContains(response, 'a good body')
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A good title")
+        self.assertTemplateUsed(response, "post_detail.html")
 
-    def test_create_page(self):
-        response = self.client.post(reverse('create'), {
-            'title':'a new title',
-            'body': 'a new body',
-            'author': self.user.id }
-                                       )
+    def test_post_createview(self):  # new
+        response = self.client.post( #here we send post request to create
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302) #302 to redirect our page to login
+        self.assertEqual(Post.objects.last().title, "New title") #here we check from Post model
+        self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_updateview(self):  # new
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(Post.objects.last().title, 'a new title')
-        self.assertEqual(Post.objects.last().body, 'a new body')
-        
-    def test_update_page(self):
-        response = self.client.post(reverse('update', args = '1'),{
-            'title':'updated one',
-            'body':'updated body' }
-                                       )
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteview(self):  # new
+        response = self.client.post(reverse("post_delete", args="1"))
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(Post.objects.last().title, 'updated one')
-        self.assertEqual(Post.objects.last().body, 'updated body')
-
-    def test_delete_page(self):
-        response = self.client.post(reverse('delete', args = '1'))
-        self.assertEqual(response.status_code, 302)
-
-
 
--- django4.0.1/django_project/settings.py
+++ django4.0.1/django_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR/"staticfiles"
+STATIC_ROOT = BASE_DIR/"staticfiles" #absolute location of collected files 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage'
 
