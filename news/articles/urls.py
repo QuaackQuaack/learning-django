3640abbea76f9f118aa83a627a14fc78abde1112
Arjun <iamarjunkharal@gmail.com>
2023-05-21 22:35:39 +0545

    build: edit, update, add, template with url management

---
+++ news/articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ArticleListView
+
+urlpatterns = [
+        path("", ArticleListView.as_view(), name= 'article_list'),
+        path("<int:pk>", ArticleDetailView.as_view(), name = 'article_page'),
+        path("<int:pk>/update", ArticleUpdateView.as_view(), name='Update'),
+        path("<int:pk>/delete", ArticleDeleteView.as_view(), name = 'delete'),
+        path("new/", ArticleCreateView.as_view(), name = 'create')
+        ]
--- news/articles/views.py
+++ news/articles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
+from .models import Article
 # Create your views here.
+
+class ArticleListView:
+    template_name = 'article_list.html'
+    model = Article
+    custom_user_name = 'articlelist'
+
+class ArticleDetailView:
+    template_name = 'article_detail.html'
+    model = Article
+    custom_user_name = 'articledetail'
+
+class ArticleUpdateView:
+    template_name = 'article_edit.html'
+    model = Article
+    fields = ( 'title', 'body' )
+
+class ArticleDeleteView:
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list') #redirect to home page
+
+class ArticleCreateView:
+    model = Article
+    template_name = 'article_create.html'
+    fields = ( 'author', 'title', 'body')
+
